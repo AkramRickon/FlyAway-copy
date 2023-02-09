aec17d8ce9400864d5a5b2a3eda5134e0d0aaffb
Akram Hossain Rickon <Akramhsin29@gmail.com>
2023-02-09 11:33:47 +0600

    Delete server directory

--- server/models/flightSegment.js
+++
@@ -1,8 +0,0 @@
-const { mongoose } = require('../db');
-const FlightDetailSchema=require('./flightDetail');
-
-const FlightSegmentsSchema = mongoose.Schema({
-    departure:FlightDetailSchema,
-    arrival: FlightDetailSchema
-})
-module.exports = FlightSegmentsSchema ;
